446139e98db72433c0e796bd7a371dcbe70669a0
divyabisht2006 <divyabishthld00@gmail.com>
2026-06-01 17:52:34 +0530

    Add solution for maximum profit from stock prices

---
+++ Week_1/Day_1/BestTimeToSellAndBuyStock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+
+            }
+            int currentprofit=prices[i]-buy;
+            if(currentprofit > profit){
+                profit=currentprofit;
+
+            }
+        }return profit;
+    }
+}
